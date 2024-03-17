31f332233ffcefbb005fd01cf266bb7ee33c8fab
Mikhail Skliar <skliarmikhail@gmail.com>
2024-03-17 14:11:15 +0300

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "pages/Home"
+import NotFound from "pages/NotFound"
+import HomeService from "services/HomeService"
+
+export const HOME_PATH = "/"
+export const NOT_FOUND_PATH = "*"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={HOME_PATH}
+            element={<Home homeService={new HomeService()} />}
+          />
+          <Route path={NOT_FOUND_PATH} element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
